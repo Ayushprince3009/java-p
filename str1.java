220a2cca5404ec146aa6f9578727161173db0623
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-27 23:46:30 +0530

    new

---
+++ str1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class str1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int num = sc.nextInt();
+        //code for start pattern
+
+        for(int i=1; i<=num; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
