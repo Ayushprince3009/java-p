05f2ddde797f48583fa4abea62651e8d8590dd52
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-28 01:03:43 +0530

    new

--- factor.java
+++ factor.java
@@ -1,2 +1,28 @@
+import java.util.*;
 public class factor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number:");
+        int num = sc.nextInt();
+        factorial(num);
+    }
+
+    public static void factorial(int a){
+        int res = 1;
+        if(a == 0){
+            System.out.println(res);
+        }
+        else if(a<0){
+            System.out.println("Invalid Number");
+        }
+        else{
+            while(a>0){
+                res = res *a;
+                a--;
+            }
+            System.out.println(res);
+        }
+
+    }
+
 }
