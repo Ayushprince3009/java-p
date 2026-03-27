87b1545a4727ae3e0c9d74a4dadd7d9d819e9b10
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-28 00:04:05 +0530

    new

---
+++ str2.java
@@ -0,0 +1,12 @@
+public class str2{
+    public static void main(String[] args) {
+        int num = 5;
+
+        for(int i=1; i<=num; i++){
+            for(int j=num; j>=i; j--){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
