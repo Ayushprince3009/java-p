c97e177a89dd2196884b1880444b9e6b87322770
Ayush Kumar <ayushkkumar840@gmail.com>
2026-03-29 02:00:27 +0530

    q4

---
+++ q4.java
@@ -0,0 +1,15 @@
+public class q4 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 4;
+
+        int result = Math.min(a,b);
+        int result1 = Math.max(a,b);
+
+        System.out.println(Math.sqrt(4)); // this will give result in decimal always
+        System.out.println(result1);
+        System.out.println(result);
+        System.out.println(Math.abs(-567));
+
+    }
+}
